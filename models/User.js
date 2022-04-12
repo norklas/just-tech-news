e0594a66f4bbc424f1d55f030f27de070757564a
Nick Melanson <nickm890@gmail.com>
2022-04-12 17:52:59 -0400

    hashed create and updated passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 // create our User models
 class User extends Model {}
@@ -7,55 +8,53 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
   {
-    // define an id column
     id: {
-      // use the special Sequelize DataTypes object to provide what type of data it instead
       type: DataTypes.INTEGER,
-      // this is the equivalent of SQL's 'NOT NULL' option
       allowNull: false,
-      // instruct that this is the Primary Key
       primaryKey: true,
-      // turn on auto increment
       autoIncrement: true,
     },
-    // define username column
     username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // define email column
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // there cannot be any duplicate email values in this table
       unique: true,
-      // if allowNull is set to false, we can run our data through validators before creating the table data
       validate: {
         isEmail: true,
       },
     },
-    // define password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least four characters long
         len: [4],
       },
     },
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
 
-    // pass in our imported sequelize connection
+        return updatedUserData;
+      },
+    },
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: "user",
   }
 );
