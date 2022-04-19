ab3478013c9fbffeec601c24a41b9514c8a857d3
Nick Melanson <nickm890@gmail.com>
2022-04-19 16:20:08 -0400

    populated the template

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,12 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create connection to our database, pass in your mySQL information for username and password
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
